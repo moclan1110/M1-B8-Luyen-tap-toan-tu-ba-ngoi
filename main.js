82d9640a53caa179b8fe232b5d2cef3333833191
moclan110 <vulan11101997@gmail.com>
2022-11-10 08:50:49 +0700

    luyen tap toan tư ba ngoi

---
+++ main.js
@@ -0,0 +1,16 @@
+// Bài 1
+// let a = +prompt('Enter number a', '');
+// let b = +prompt('Enter number b', '');
+// result = (a+b)<4? 'Below' : 'Over';
+// document.write(result);
+
+// Bài 2
+// let login = prompt('Enter:');
+// let message = (login == 'Employee')?
+//     'Hello':
+//     (login == 'Director')?
+//         'Greetings':
+//         (login == '')?
+//             'No login':
+//             '';
+// alert(message);
